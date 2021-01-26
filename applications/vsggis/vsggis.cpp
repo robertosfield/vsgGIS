2e83218d4c1372268d0f8026a49da3487e4b1135
Robert Osfield <robert@openscenegraph.com>
2021-01-26 12:45:51 +0000

    Added initial shell of vsggis test applications

---
+++ applications/vsggis/vsggis.cpp
@@ -0,0 +1,15 @@
+#include <vsg/all.h>
+
+#include <chrono>
+#include <iostream>
+#include <ostream>
+#include <thread>
+
+int main(int argc, char** argv)
+{
+    vsg::CommandLine arguments(&argc, argv);
+
+    std::cout<<"vsggis"<<std::endl;
+
+    return 0;
+}
